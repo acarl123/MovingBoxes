51a748bb88b21ac0e372942b91012a4e0201b9e0
acarl123 <chuckles.theman@gmail.com>
2014-05-23 13:41:53 -0400

    drawing 100 frames in pygame and floatcanvas

--- FloatCanvas/numberTest.py
+++ FloatCanvas/numberTest.py
@@ -1,10 +1,14 @@
 import wx, random, time
-from wx.lib.floatcanvas import NavCanvas
+from wx.lib.floatcanvas import NavCanvas, FloatCanvas
 
 app = wx.App(False)
 N_RECTANGLES = 2500
+N_FRAMES = 100
 blue = 100, 223, 237
 start = time.time()
+
+def onRectHit():
+   pass
 
 frame = wx.Frame(None, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 1024,768 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 bSizer1 = wx.BoxSizer(wx.VERTICAL)
@@ -16,13 +20,21 @@ bSizer1.Add(NC, 1, wx.EXPAND | wx.ALL, 5)
 frame.SetSizer(bSizer1)
 frame.Layout()
 frame.Centre(wx.BOTH)
-
-for i in range(N_RECTANGLES):
-   x = random.randint(0, 900)
-   y = random.randint(0, 600)
-   Canvas.AddRectangle(Canvas.PixelToWorld((x,y)), (80, 40), LineWidth=2, FillColor=blue)
-   Canvas.AddText('Hello There', Canvas.PixelToWorld((x+1, y-30)), 8)
-
 frame.Show()
+
+
+for x in xrange(N_FRAMES):
+   Canvas.Draw()
+   Canvas.UnBindAll()
+   Canvas.ClearAll()
+
+   for i in range(N_RECTANGLES):
+      x = random.randint(0, 900)
+      y = random.randint(0, 600)
+      rect = Canvas.AddRectangle(Canvas.PixelToWorld((x,y)), (80, 40), LineWidth=2, FillColor=blue)
+      Canvas.AddText('Hello There', Canvas.PixelToWorld((x+1, y-30)), 8)
+      rect.Bind(FloatCanvas.EVT_FC_LEFT_DOWN, onRectHit)
+
 print time.time() - start
-app.MainLoop()
+
+
--- pygame squares/numberTest.py
+++ pygame squares/numberTest.py
@@ -6,8 +6,11 @@ screen = pygame.display.set_mode(size, pygame.DOUBLEBUF | pygame.RESIZABLE)
 random.seed()
 
 N_RECTANGLES = 2500
+N_FRAMES = 100
 blue = 100, 223, 237
 black = 0, 0, 0
+need_screen_update = True
+frameCounter = 0
 
 start = time.time()
 
@@ -15,36 +18,41 @@ font = pygame.font.Font(None, 16)
 text = font.render("Hello There", 1, (10, 10, 10))
 textpos = text.get_rect()
 
-
-for i in range(N_RECTANGLES):
-   x = random.randint(0, 900)
-   y = random.randint(0, 600)
-   pygame.draw.rect(screen,
-                        blue,
-                        (x,
-                        y,
-                        80,
-                        40),
-                        )
-
-   pygame.draw.rect(screen,
-                    black,
-                    (x,
-                     y,
-                     80,
-                     40,),
-                    2,
-                    )
-   textpos.centerx = x+40
-   textpos.centery = y+20
-   screen.blit(text, textpos)
-
-
-
-pygame.display.update()
-print time.time() - start
-
-while 1:
+while frameCounter < N_FRAMES:
+   frameCounter += 1
    for event in pygame.event.get():
       if event.type == pygame.QUIT:
          sys.exit(0)
+
+      if event.type == pygame.MOUSEBUTTONDOWN:
+         need_screen_update = True
+
+   if need_screen_update:
+      screen.fill(black)
+      # need_screen_update = False
+      for i in range(N_RECTANGLES):
+         x = random.randint(0, 900)
+         y = random.randint(0, 600)
+         pygame.draw.rect(screen,
+                          blue,
+                          (x,
+                           y,
+                           80,
+                           40),
+         )
+
+         pygame.draw.rect(screen,
+                          black,
+                          (x,
+                           y,
+                           80,
+                           40,),
+                           2,
+         )
+         textpos.centerx = x + 40
+         textpos.centery = y + 20
+         screen.blit(text, textpos)
+
+   pygame.display.update()
+
+print time.time() - start
