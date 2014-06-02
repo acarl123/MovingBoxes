bd9c7b01579715796cd453e1d196bc86b4daa1de
acarl123 <chuckles.theman@gmail.com>
2014-06-02 10:22:23 -0400

    working on custom class

--- NavigatorBeta/NavigatorFloatCanvas.py
+++ NavigatorBeta/NavigatorFloatCanvas.py
@@ -6,4 +6,3 @@ class NavigatorFloatCanvas( NavCanvas.NavCanvas ):
    def __init__(self, parent, id=wx.ID_ANY, size=wx.DefaultSize, **kwargs):
       self.navCanvas = NavCanvas.NavCanvas
       self.navCanvas.__init__(self, parent, id, size, **kwargs)
-      pass
