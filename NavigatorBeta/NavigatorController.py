44b07f4171aac1991c7a19375e8270aa9a0632cf
jonesm33 <jonesm33@xavier.edu>
2014-06-02 14:27:46 -0400

    Working on Mouse events

--- NavigatorBeta/NavigatorController.py
+++ NavigatorBeta/NavigatorController.py
@@ -13,10 +13,15 @@ class NavigatorController:
       self.mainWindow.Bind(wx.EVT_MOUSE_EVENTS, self.onMouse) # Add all mouse events to a single bind
 
       # Initialize member variables
+      self.rects = {}
+      self.selectedRects = []
+      self.collidedRects = []
       self.mousePositions = deque([])
       self.mouseRel = 0, 0
 
-   """ Bindings """
+   #--------------------------------------------------------------------------------------#
+   # Bindings
+   #--------------------------------------------------------------------------------------#
    def onExit(self, event):
       self.mainWindow.Destroy()
       exit()
@@ -28,3 +33,25 @@ class NavigatorController:
       if len(self.mousePositions) == 2:
          self.mouseRel = ((self.mousePositions[1][0] - self.mousePositions[0][0]), (self.mousePositions[1][1] - self.mousePositions[0][1]))
          self.mousePositions.popleft()
+
+      if event.GetEventType() == wx.wxEVT_LEFT_DOWN: self.onClick(event)
+      if event.GetEventType() == wx.wxEVT_RIGHT_DOWN: self.onRClick(event)
+      if event.GetEventType() == wx.wxEVT_LEFT_UP: self.onLUp(event)
+      if event.Dragging() == True: self.onDrag(event)
+
+   def onClick(self, event):
+      # Set a black border rectangle on each shape
+      for rectNum in self.rects:
+         self.rects[rectNum][0].SetLineColor(wx.Colour(0, 0, 0))
+      self.selectedRects = []
+
+      print 'foo'
+
+   def onRClick(self, event):
+      print 'foo'
+
+   def onLUp(self, event):
+      print 'foo'
+
+   def onDrag(self, event):
+      print 'foo'
--- NavigatorBeta/NavigatorView.py
+++ NavigatorBeta/NavigatorView.py
@@ -15,7 +15,7 @@ from NavigatorFloatCanvas import NavigatorFloatCanvas
 ## Class MyFrame1
 ###########################################################################
 
-class MyFrame1 ( wx.Frame ):
+class NavigatorFrame ( wx.Frame ):
 
 	def __init__( self, parent ):
 		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 725,558 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
@@ -86,9 +86,6 @@ class MyFrame1 ( wx.Frame ):
 
 		self.m_statusBar1 = self.CreateStatusBar( 1, wx.ST_SIZEGRIP, wx.ID_ANY )
 		bSizer1 = wx.BoxSizer( wx.VERTICAL )
-
-		self.m_panel1 = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
-		bSizer1.Add( self.m_panel1, 1, wx.EXPAND |wx.ALL, 5 )
 
 		self.NavCanvas = NavigatorFloatCanvas(self, Debug=0, BackgroundColor=(173, 173, 173))
 		bSizer1.Add( self.NavCanvas, 1, wx.ALL|wx.EXPAND, 5 )
